4caf12862e76aa34787981a301ff63e5d9e9974f
llucero <clucero3696@gmail.com>
2024-06-05 20:07:58 -0700

    June Update

--- app/projectdata.js
+++ app/projectdata.js
@@ -1,37 +1,22 @@
 
 const projects = [
   {
-    name: 'Jet Boat Classifieds',
-    description: 'E-Commerce business application. React, Firebase, & Stripe payment processing.',
-    src: '/shopScreen.PNG',
-    link: 'https://jetboats.netlify.app/',
-    git: 'https://github.com/clucero36/jetboats.git',
+    name: 'Ayamaya Outdoor',
+    description: 'E-Commerce website built with Shopify.',
+    src: '/ayamaya-homepage.webp',
+    link: 'https://ayamaya.com',
   },
   { 
     name: 'Spotify Clone',
     description: 'Spotify Clone Web Application. React, Spotify OAuth 2.0, & Firebase.',
-    src: '/spot1stPage.PNG',
+    src: '/spot1stPage.webp',
     link: 'https://spotifyditto.netlify.app',
     git: 'https://github.com/clucero36/spotify-clone.git',
   },
   {
-    name: 'Know The Truth',
-    description: 'Website built with Shopify CMS.',
-    src: '/ktt.PNG',
-    link: 'https://f3fd82.myshopify.com/',
-    git: '#'
-  },
-  {
-    name: 'Salon WebApp',
-    description: 'Business Web App. Bookings Coming Soon. Next.js, Node/Express, ChakraUI.',
-    src: '/salon-webapp.PNG',
-    link: 'https://salon-webapp.vercel.app',
-    git: 'https://github.com/clucero36/salon-webapp.git'
-  },
-  {
     name: 'Blogger Landing Page',
     description: 'Business landing page. HTML, CSS, & JavsScript.',
-    src: '/bloggrNew.PNG',
+    src: '/bloggrNew.webp',
     link: 'https://blogrbusiness.netlify.app/',
     git: 'https://github.com/clucero36/Blogr.git',
   },
